3b065e9f6b612aba591e6d0384aa132c65b03a6f
TenzinWangden <129078538+TenzinWangden@users.noreply.github.com>
2023-06-26 14:10:09 -0400

    daily update

--- Day 4/01review.js
+++ Day 4/01review.js
@@ -1,25 +1,31 @@
-/** 
- * truthy vs falsy
+console.log("hello");
+/**
+ * Which of these are truthy vs falsy
  */
-// let x = 'a'; // truthy
-// x = ''; //falsy
-// x=[];//truthy due to object
-// x=x.length;//falsy due to x length being (0)
-// x={}; //truthy due to object
-// let y; //falsy
-// x=y; // falsy due to not defined
 
-// x = Number("0"); // is this a strig of 0 => truthy? explicit casting will turn it into a number => fals
-// x = Number("hello"); // falsy casting to number and passed invalid number string 
-// x = Boolean("false"); // Boolean creates a Boolean based on the paramter's truthy or falsy state
-// x = Boolean(""); // Boolean creates a Boolean based on the paramter's truthy or falsy state
+// In js we have truthy and falsy not just true and false
+let x = 'a'; // truthy
+x = ''; // falsy
+x = []; // truthy
+x = x.length; // is this null or undefined =>falsy
+x = {}; // truthy
+let y; // NOTE: declaring and not initialzing sets the value to undefined
+x = y; // falsy
+x = "0"; // truthy
+
+x = Number("0"); // is this a strig of 0 => truthy? explicit casting will turn it into a number => fals
+x = Number("hello"); // falsy casting to number and passed invalid number string 
+x = Boolean("false"); // Boolean creates a Boolean based on the paramter's truthy or falsy state
+x = Boolean(""); // Boolean creates a Boolean based on the paramter's truthy or falsy state
 
 // x = NaN; // falsy
-// x = null; // null is a placeholder -- falsy 
+x = null; // null is a placeholder -- falsy 
 // x = null == undefined; // loosely speaking they are 
 // x = null === undefined; // strictly speaking these aren't the same
-// x = -0; // falsy
-// let t = typeof x;
+x = -0; // falsy
+
+
+let t = typeof x;
 console.log(`x is currently ${x} and is of type ${t}`);
 
 if (x) {
@@ -34,6 +40,8 @@ console.log("good bye");
  * ----------------------
  * - '' empty string
  * - 0
+ * - -0
+ * - 0n is BigInt value of 0 (BigInt = arbitrary large number uses however many bits it needs so never underflows/ overlows)
  * - false
  * - undefined
  * - null
---
+++ Day 4/06A/asynch.js
@@ -0,0 +1,6 @@
+let numDiv = document.getElementById("num")
+let x = 0;
+numDiv.textContent = "No value set";
+setTimeout(()=>{
+    numDiv.textContent = 10;
+}, 3000);
